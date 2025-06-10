387d47087390774dca56a9580c2d0aa580781a12
Promise Ekhosuehi <ekhosuehipromise@gmail.com>
2025-06-10 19:15:15 +0100

    update surmarizetion model

--- src/utils/surmarize.ts
+++ src/utils/surmarize.ts
@@ -1,6 +1,9 @@
 import { InferenceClient } from '@huggingface/inference';
 import config from '../config/config';
 
+
+import { pipeline } from '@xenova/transformers';
+
 const hf = new InferenceClient(config.hfKey.key as string);
 
 export function chunkText(text: string, maxWords = 500): string [] {
@@ -16,21 +19,37 @@ export function chunkText(text: string, maxWords = 500): string [] {
 /**
  * Summarizes a block of text using the distilBART model
  */
-export async function summarizeText(text: string): Promise<string> {
-  try {
-    const summary = await hf.summarization({
-      model: 'sshleifer/distilbart-cnn-12-6',
-      inputs: text,
-      parameters: {
-        max_length: 150,
-        min_length: 30,
-        do_sample: false,
-      },
-    });
 
-    return summary.summary_text;
-  } catch (error) {
-    console.error('Summarization error:', error);
-    throw new Error('Failed to summarize text.');
-  }
-}
+
+
+export const runSummarizer = async (chunk: string) => {
+  const summarizer = await pipeline('summarization', 'Xenova/t5-small');
+
+  const result = await summarizer(chunk, {
+    max_length: 50,
+    min_length: 5,
+  });
+
+  console.log('Summary:', result[0]);
+};
+
+
+
+// export async function summarizeText(text: string): Promise<string> {
+//   try {
+//     const summary = await hf.summarization({
+//       model: 'sshleifer/distilbart-cnn-12-6',
+//       inputs: text,
+//       parameters: {
+//         max_length: 150,
+//         min_length: 30,
+//         do_sample: false,
+//       },
+//     });
+
+//     return summary.summary_text;
+//   } catch (error) {
+//     console.error('Summarization error:', error);
+//     throw new Error('Failed to summarize text.');
+//   }
+// }
